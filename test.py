658b85cd9a172aa7915c86d834f53dadf6f59476
johannesjmeyer <jj.meyer@outlook.com>
2021-11-10 14:01:04 +0100

    Add ttt plot

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
 import numpy as np
 import tictactoe as ttt
+import matplotlib.pyplot as plt
 
 game = np.array([[-1, 1, 1], [0, -1, 1], [-1, 0, -1]])
 game2 = np.array([[1, 1, 1], [-1, -1, -1], [0, 0, 0]])
 
 games, labels = ttt.get_data()
+
+fig = plt.figure()
+ttt.plot(game, plt.gca())
+plt.show()
--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,6 @@
 import numpy as np
 import itertools
+import matplotlib.pyplot as plt
 
 MAGIC_SQUARE = np.array([[8, 1, 6], [3, 5, 7], [4, 9, 2]])
 
@@ -70,3 +71,23 @@ def get_data():
     print("Data loading complete")
 
     return all_games, all_labels
+
+def plot(game, ax):
+    ax.set_xlim(0, 3)
+    ax.set_ylim(0, 3)
+    ax.axvline(1, color="gray")
+    ax.axvline(2, color="gray")
+    ax.axhline(1, color="gray")
+    ax.axhline(2, color="gray")
+
+    for idx in np.ndindex(*game.shape):
+        val = game[idx]
+        if val == 1:
+            # ax.scatter(idx[0] + .5, idx[1] + .5, marker="o", lw=8, s=4000, facecolors="none", edgecolors="C1")                    
+            ax.add_patch(plt.Circle((idx[0] + .5, idx[1] + .5), 0.3, color='C1', lw=8, fill=False))
+        elif val == -1:
+            D = 0.27
+            ax.plot([idx[0] + .5 - D, idx[0] + .5 + D], [idx[1] + .5 - D, idx[1] + .5 + D], lw=8, color="C0")
+            ax.plot([idx[0] + .5 - D, idx[0] + .5 + D], [idx[1] + .5 + D, idx[1] + .5 - D], lw=8, color="C0")
+
+    ax.axis("off")
