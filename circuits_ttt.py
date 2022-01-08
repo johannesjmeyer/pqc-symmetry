a351a2ecbba9c8d8ec8f6bd632e92ca26fdc0c06
Marian Mularski <marian.mularski@porsche.digital>
2022-01-08 20:55:25 +0100

    implement circuit design as parameter string, clean up code

--- circuits_ttt.py
+++ circuits_ttt.py
@@ -19,6 +19,8 @@ ttt_dev = qml.device("default.qubit", wires=9) # the device used to label ttt in
 ###################################################
 ###################################################
 ###################################################
+args_symmetric = {'c': 2, 'e': 2, 'o': 1, 'm': 2, 'i': 1, 'd': 1}
+args_asymmetric = {'c': 8, 'e': 8, 'o': 8, 'm': 2, 'i': 4, 'd': 4}
 
 def data_encoding(game):
     '''
@@ -39,7 +41,9 @@ def data_encoding(game):
 
 ### Symmetric functions ###
 def corners(param, symm=True):
-
+    '''
+    applies RX an RZ gates on corners
+    '''
     qubits = [0, 2, 4, 6]
 
     if symm:
@@ -56,7 +60,9 @@ def corners(param, symm=True):
     
      # TODO: add ry
 def edges(param, symm=True):
-
+    '''
+    applies RX an RZ gates on edges
+    '''
     qubits = [1, 3, 5, 7]
 
     if symm:
@@ -72,20 +78,22 @@ def edges(param, symm=True):
     
     
 def center(param):
- 
+    '''
+    applies RX an RZ gate on the middle qubit
+    '''
     qml.RX(param[0], wires=8)
     qml.RY(param[1], wires=8)
 
 
 def outer_layer(param, symm=True):
     '''
+    entangles nearest neighboar qubits of the outer layer of the tictactoe board
     0  - 1 -  2
     |         |
     7    8    3
     |         |
     6  - 5 -  4
     '''
-
     connections = list(range(8)) + [0] 
 
     if symm:
@@ -98,6 +106,7 @@ def outer_layer(param, symm=True):
 
 def inner_layer(param, symm=True):
     '''
+    entangles center qubit with edge qubits
     0    1    2
          |
     7  - 8 -  3
@@ -116,6 +125,7 @@ def inner_layer(param, symm=True):
   
 def diag_layer(param, symm=True):
     '''
+    entangles center qubit with corner qubits
     0    1    2 
       \     /
     7    8    3
@@ -164,85 +174,104 @@ def column_layer(params):
             qml.CRZ(params[2*row+col],wires=[3*col+row,3*col+row+1])
 
 
+def translate_to_parameters(design, symmetric=True):
+    '''
+    Translates circuit design string to number of required parameters in the form of a list corresponding to each applied layer
+    '''
+    param_args = [0]
+    if symmetric:
 
+        for i in design.replace(" ", ""):
+            if i == 't':
+                # encode data
+                param_args.append(param_args[-1])
+            elif i == 'c':
+                # corners
+                param_args.append(param_args[-1]+args_symmetric['c'])
+            elif i == 'e':
+                # edges
+                param_args.append(param_args[-1]+args_symmetric['e'])
+            elif i == 'o':
+                # outer layer
+                param_args.append(param_args[-1]+args_symmetric['o'])
+            elif i == 'm':
+                # center
+                param_args.append(param_args[-1]+args_symmetric['m'])
+            elif i == 'i':
+                # inner layer
+                param_args.append(param_args[-1]+args_symmetric['i'])
+            elif i == 'd':
+                # diagonal layer
+                param_args.append(param_args[-1]+args_symmetric['d'])
+    else:
+        
+        for i in design.replace(" ", ""):
+            if i == 't':
+                # encode data
+                param_args.append(param_args[-1])
+            elif i == 'c':
+                # corners
+                param_args.append(param_args[-1]+args_asymmetric['c'])
+            elif i == 'e':
+                # edges
+                param_args.append(param_args[-1]+args_asymmetric['e'])
+            elif i == 'o':
+                # outer layer
+                param_args.append(param_args[-1]+args_asymmetric['o'])
+            elif i == 'm':
+                # center
+                param_args.append(param_args[-1]+args_asymmetric['m'])
+            elif i == 'i':
+                # inner layer
+                param_args.append(param_args[-1]+args_asymmetric['i'])
+            elif i == 'd':
+                # diagonal layer
+                param_args.append(param_args[-1]+args_asymmetric['d'])
+        
 
+    return param_args
+# %%
 #@qml.qnode(ttt_dev, interface='torch')
-def circuit(game, params, symmetric):
-        #params_single = params[0]
-        #params_multi = params[1]
-
+def circuit(game, params, symmetric, design="tceocem tceicem tcedcem"):
         '''
         prepares the all zero comp basis state then iterates through encoding and layers
-        input: params, np array of shape r x 2 x 6 
+        encoding and layers are defined by design argument
+            game:   game as 3x3 arraa
+            params: tensor of parameters
+            symmetric: bool
+            design: string specifying order of encoding layers
+                    t: encode game
+                    c: corners
+                    e: edges
+                    m: middle/center
+                    o: outer layer
+                    i: inner layer
+                    d: diagonal layer
         '''
         # TODO: this should automatically start from the all-zero state in comp basis right?
 
+        args = translate_to_parameters(design)
         ngame = np.pi*0.5*game # normalize entries of game so they are between -pi/2, pi/2
 
-        for r in range(params.shape[0]): # for each of the r repetitions interleave data_enc with row and colum n layers
-            #print('r {}'.format(r))
+        for r in range(params.shape[0]): # r repetitions
+       
 
-            if symmetric:
-                data_encoding(ngame)
+            for i, n in enumerate(design.replace(" ", "")):
+                if i == 't':
+                    data_encoding(ngame)
+                elif i == 'c':
+                    corners(params[r, args[n]:args[n+1]], symmetric)
+                elif i == 'e':
+                    edges(params[r, args[n]:args[n+1]], symmetric)
+                elif i == 'o':
+                    outer_layer(params[r, args[n]:args[n+1]], symmetric) 
+                elif i == 'm':
+                    center(params[r, args[n]:args[n+1]])
+                elif i == 'i':
+                    inner_layer(params[r, args[n]:args[n+1]], symmetric) 
+                elif i == 'd':
+                    diag_layer(params[r, args[n]:args[n+1]], symmetric)
 
-                corners(params[r, 0, 0])
-                edges(params[r, 0, 1])
-                outer_layer(params[r, 0, 5, 0]) 
-                corners(params[r, 0, 2])
-                edges(params[r, 0, 3])
-                center(params[r, 0, 4])
-
-
-                data_encoding(ngame)
-                corners(params[r, 1, 0])
-                edges(params[r, 1, 1])
-                inner_layer(params[r, 1, 5, 0]) 
-                corners(params[r, 1, 2])
-                edges(params[r, 1, 3])
-                center(params[r, 1, 4])
-
-                data_encoding(ngame)
-                corners(params[r, 2, 0])
-                edges(params[r, 2, 1])
-                diag_layer(params[r, 2, 5, 0]) 
-                corners(params[r, 2, 2])
-                edges(params[r, 2, 3])
-                center(params[r, 2, 4])
-
-            else: 
-                data_encoding(ngame)
-                corners(params[r, 0:8], False) 
-                edges(params[r, 8:16], False) 
-                outer_layer(params[r, 16:24], False) 
-                corners(params[r, 24:32], False) 
-                edges(params[r, 32:40], False) 
-                center(params[r, 40:42])
-
-
-                data_encoding(ngame)
-                corners(params[r, 42:50], False) 
-                edges(params[r, 50:58], False) 
-                inner_layer(params[r, 58:62], False) 
-                corners(params[r, 62:70], False) 
-                edges(params[r, 70:78], False) 
-                center(params[r, 78:80])
-
-                data_encoding(ngame)
-                corners(params[r, 80:88], False) 
-                edges(params[r, 88:96], False) 
-                diag_layer(params[r, 96:100], False) 
-                corners(params[r, 100:108], False) 
-                edges(params[r, 108:116], False) 
-                center(params[r, 116:118]) 
-
-            ### old stuff ###
-            #drawer = qml.d
-            # raw(data_encoding)
-            #print(drawer(ngame))
-            #row_layer(params[r,0])
-            #drawer = qml.draw(row_layer)
-            #print(drawer(params[r,0]))
-            #column_layer(params[r,1])
 
         return qml.expval(qml.PauliZ(8)) # measure one qubit in comp basis
 
@@ -252,59 +281,33 @@ def circuit(game, params, symmetric):
 full_circ = qml.QNode(circuit, ttt_dev)
 full_circ_torch = qml.QNode(circuit, ttt_dev, interface='torch')
 #full_circ_jax = qml.QNode(circuit, ttt_dev, interface='jax')
-
 #full_circ = jax.jit(full_circ_jax)
 
 ###################################################
 ###################################################
 ###################################################
 
-
-#game = np.array([[-1, 1, 1], [0, -1, 1], [-1, 0, -1]], requires_grad = False) # just a random game
-
-
-
-
 rng = np.random.default_rng(2021)
-#print(params)
-#print([params])
-
-
-
-#c = full_circ(game, params)
-
-#drawer = qml.draw(full_circ)
-#print(drawer(game, params))
 
 #TODO train something on a few labelled data!
 
 ############################
 
+def random_params(repetitions, symmetric, design, torch=False):
+    '''
+    returns array/torch tensor of paramters for amount of repetitions and circuit design
+    '''
+    params = rng.uniform(low=-1, high=1, size=(repetitions,np.sum(translate_to_parameters(design, symmetric))))
+    if torch:
+        return torch.tensor(params, requires_grad = True)
+    else: 
+        return np.array(params, requires_grad = True)
 
-def random_params_old(repetitions, symmetric):
-    if symmetric:
-        return np.array(rng.uniform(low=-1, high=1, size=(repetitions,2,3)), requires_grad = True)
-    else:
-        return np.wrap_arrays(rng.uniform(low=-1, high=1, size=(repetitions,8+4+4+4+4+1)), requires_grad = True)
-
-def random_params(repetitions, symmetric):
-    if symmetric:
-        #param_single = torch.tensor(rng.uniform(low=-1, high=1, size=(repetitions,3,5,2)), requires_grad = True)
-        #param_multi = torch.tensor(rng.uniform(low=-1, high=1, size=(repetitions,3)), requires_grad = True)
-        #return [param_single, param_multi]
-        return np.array(rng.uniform(low=-1, high=1, size=(repetitions,3,6,2)), requires_grad = True)
-    else:
-        return np.array(rng.uniform(low=-1, high=1, size=(repetitions,118)), requires_grad = True)
-
-def random_params_torch(repetitions, symmetric):
-    if symmetric:
-        #param_single = torch.tensor(rng.uniform(low=-1, high=1, size=(repetitions,3,5,2)), requires_grad = True)
-        #param_multi = torch.tensor(rng.uniform(low=-1, high=1, size=(repetitions,3)), requires_grad = True)
-        #return [param_single, param_multi]
-        return torch.tensor(rng.uniform(low=-1, high=1, size=(repetitions,3,6,2)), requires_grad = True)
-    else:
-        return torch.tensor(rng.uniform(low=-1, high=1, size=(repetitions,118)), requires_grad = True)
-
+def random_params_torch(repetitions, symmetric, design):
+    '''
+    returns torch tensor of paramters for amount of repetitions and circuit design
+    '''
+    return torch.tensor(rng.uniform(low=-1, high=1, size=(repetitions,np.sum(translate_to_parameters(design, symmetric)))), requires_grad = True)
 
 def cost_function(params,game, symmetric):
     return (full_circ(game,params, symmetric)-get_label(game))**2
@@ -314,7 +317,6 @@ def cost_function_batch(params,games_batch, symmetric):
     normalized least squares cost function over batch of data points (games)
     '''
     return sum([(full_circ(g,params, symmetric)-get_label(g))**2 for g in games_batch])/np.shape(games_batch)[0]
-
 
 def cost_function_torch(params,game, symmetric):
     return (full_circ_torch(game,params, symmetric)-get_label(game))**2
@@ -345,12 +347,14 @@ def gen_games_sample(size, wins=[1, 0, -1], output = None):
 
 
     return np.tensor(sample, requires_grad=False), np.tensor(sample_label, requires_grad=False)
+
 class tictactoe():
 
-    def __init__(self, symmetric=True, sample_size=5, data_file=None):
+    def __init__(self, symmetric=True, sample_size=5, design="tceocem tceicem tcedcem", data_file=None):
         #self.opt = qml.GradientDescentOptimizer(0.01)
         self.sample_size = sample_size
-        
+        self.design = design
+
         if data_file == None:
             self.sample_games(sample_size)
         else:
@@ -359,23 +363,25 @@ class tictactoe():
         self.symmetric = symmetric
 
     def random_parameters(self, size=1, repetitions=2, torch=True):
+        '''
+        sets random parameters for circuit design and amount of repetitions
+        '''
         if size==1:
-            if torch:
-                self.init_params = random_params_torch(repetitions, self.symmetric)
-            else:
-                self.init_params = random_params(repetitions, self.symmetric)
+            self.init_params_torch = random_params(repetitions, self.symmetric, self.design, True)
+            self.init_params = random_params(repetitions, self.symmetric, self.design)
         else:
             # Find best starting paramters
-            if torch:
-                params_list = [random_params_torch(repetitions, self.symmetric) for i in range(size)]
-            else: 
-                params_list = [random_params(repetitions, self.symmetric) for i in range(size)]
+            params_list = [random_params(repetitions, self.symmetric, self.design, True) for i in range(size)]
+            params_list_torch = [random_params(repetitions, self.symmetric, self.design) for i in range(size)]
 
             cost_list = [cost_function_batch(k,self.games_sample, self.symmetric) for k in params_list]
             self.init_params = params_list[np.argmin(cost_list)]
+            self.init_params_torch = params_list_torch[np.argmin(cost_list)]
 
     def sample_games(self, size):
-        # Create random samples with equal amount of wins for X, O and 0
+        '''
+        Create random samples with equal amount of wins for X, O and 0
+        '''
         self.games_sample, self.label_sample = gen_games_sample(size, wins=[-1, 0, 1])
 
     def load_games(self, data_file, size):
@@ -394,9 +400,12 @@ class tictactoe():
             self.sample_games(size)
 
 
-    def run(self, steps, resume = False):
+    def run(self, steps, stepsize=0.01, resume = False):
+        '''
+        runs qml with standard pennylane gradient descent optimizer
+        '''
         self.interface = 'pennylane'
-        self.opt = qml.GradientDescentOptimizer(0.01)
+        self.opt = qml.GradientDescentOptimizer(stepsize)
 
         if not resume:
             self.gd_cost = []
@@ -411,13 +420,16 @@ class tictactoe():
         print(self.gd_cost)
         print(self.theta)
 
-    def run_torch(self, steps, resume = False):
+    def run_lbgfs(self, steps, stepsize=0.1, resume = False):
+        '''
+        Runs qml with torch's lbfgs implementation. Usually converges much quicker than pennylanes standard gradient descent optimizer
+        '''
         self.interface = 'torch'
         if not resume:
             self.gd_cost = []
             self.theta = self.init_params
 
-        self.opt = torch.optim.LBFGS([self.theta], lr=0.1)
+        self.opt = torch.optim.LBFGS([self.theta], lr=stepsize)
 
         def closure():
             self.opt.zero_grad()
@@ -442,7 +454,9 @@ class tictactoe():
         self.theta = self.opt.param_groups[0]['params'][0]  
             
     def check_accuracy(self, check_size = 100):
-        # Check what results correspond to which label
+        '''
+        checks accuracy of current theta by sampling check_size amount of games for each win
+        '''
         games_check, labels_check = gen_games_sample(check_size)
         results = {-1: {}, 0: {}, 1: {}}
         results_alt = {-1: [], 0: [], 1: []}
@@ -471,10 +485,16 @@ class tictactoe():
         print('Accuracy for random sample of {} games: \n\n \t\t -1: {}% \n \t\t  0: {}% \n \t\t  1: {}%'.format(check_size*3, self.accuracy[-1]*100, self.accuracy[0]*100, self.accuracy[1]*100))
 
     def plot_cost(self):
+        '''
+        plots cost function
+        '''
         plt.plot(self.gd_cost)
         plt.show()
 
     def save(self, name):
+        '''
+        saves result of qml as a npy file. Can be analyzed later
+        '''
         to_save = {'symmetric': self.symmetric, 'accuracy': self.accuracy, 'steps': self.steps,  'interface': self.interface, 'cost function': self.gd_cost, 'sample size': self.sample_size, \
         'initial parameters': self.init_params.detach().numpy(), 'sampled games': self.games_sample.numpy(), 'theta': self.theta.detach().numpy()}
         #dd.io.save(name + '.h5', to_save)
@@ -504,15 +524,8 @@ plt.legend()
 plt.show
 '''
 
-
-# TODO: imply nunmpy save or deepdish H5
-
-
-# TODO: implement lbfgs optimizer (torch)
 # TODO: run on cloud/cluster
 # TODO: try different circuits
 
-
-# TODO: accuracy test?
 # TODO: enforce symmetries?
 # %%
