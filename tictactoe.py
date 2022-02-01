c06a3eb8532700ba142ab58787da20ef91844ab0
Marian Mularski <marian.mularski@porsche.digital>
2022-02-01 21:50:37 +0100

    create reduced dataset excluding games that are equivalent under the symmetry

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,9 @@
 from pennylane import numpy as np
 import itertools
 import matplotlib.pyplot as plt
+from sqlalchemy import all_
+import random
+from tqdm import tqdm
 
 MAGIC_SQUARE = np.array([[8, 1, 6], [3, 5, 7], [4, 9, 2]])
 
@@ -72,6 +75,59 @@ def get_data():
 
     return all_games, all_labels
 
+def get_data_symmetric():
+    try:
+        print("Loading games and labels but excluding equivalent games under symmetry")
+        all_games = np.load("tictactoe_games_symmetric.npy")
+        all_labels = np.load("tictactoe_labels_symmetric.npy")
+    except IOError:
+        print("No file found, creating data")
+        all_games, all_labels = make_symmetric(*get_data())
+
+        print("Saving data for future use")
+        np.save("tictactoe_games_symmetric.npy", all_games)
+        np.save("tictactoe_labels_symmetric.npy", all_labels)
+
+    print("Data loading complete")
+
+    return all_games, all_labels
+
+def make_symmetric(games, labels = None):
+    """
+    Creates new dataset excluding games that are symetrically equivalent (rotation, mirroring).
+    This is terribly inefficient but we only need to run this once
+    """
+    symmetric_games = []
+    symmetric_labels = []
+    for i, game in tqdm(enumerate(games)):
+        symmetries = get_symmetries(game)
+        
+        in_array = False
+        for g in symmetries:
+            if any((g==x).all() for x in symmetric_games):
+                in_array = True
+                break
+        if not in_array:
+            symmetric_games.append(random.choice(symmetries))
+            if labels is None:
+                symmetric_labels.append(get_label(game))
+            else:
+                symmetric_labels.append(labels[i])
+
+    return symmetric_games, symmetric_labels
+
+def get_symmetries(game):
+
+    all_symmetries = [game]      
+
+    for i in range(3):
+        all_symmetries.append(np.rot90(all_symmetries[-1]))
+
+    all_symmetries.append(np.flipud(game))
+    all_symmetries.append(np.fliplr(game))
+
+    return all_symmetries
+
 def plot(game, ax):
     ax.set_xlim(0, 3)
     ax.set_ylim(0, 3)
@@ -91,3 +147,4 @@ def plot(game, ax):
             ax.plot([idx[0] + .5 - D, idx[0] + .5 + D], [idx[1] + .5 + D, idx[1] + .5 - D], lw=8, color="C0")
 
     ax.axis("off")
+# %%
