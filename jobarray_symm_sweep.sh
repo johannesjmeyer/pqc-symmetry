310d02e9a2b907cca39e101866f6280497ee5035
Francesco Arzani <frarzani@physik.fu-berlin.de>
2022-04-13 16:55:11 +0200

    more slurm hacks

--- jobarray_symm_sweep.sh
+++ jobarray_symm_sweep.sh
@@ -11,7 +11,7 @@
 #SBATCH --mail-user=frarzani@physik.fu-berlin.de # Email to which notifications will be sent
 #SBATCH --mail-type=ALL                      # Type of email notification: BEGIN,END,FAIL,ALL
 #
-#SBATCH --array=0-19%10                  # Array of X jobs id 0..X-1, each using the allocation above, Y of which running concurrently
+#SBATCH --array=0-239                 # Array of X jobs id 0..X-1, each using the allocation above, Y of which running concurrently
                                          # REMEMBER TO CHANGE X DEPENDING ON THE PARAMETERS TO SWEEP
 
 #source /net/opt/spack/buster/spack-dev/opt/linux-debian10-ivybridge/gcc-8.3.0/lmod-8.6.5-vscnztx6cokd2oibqjvon7ykfwf4tumj/lmod/lmod/init/bash
@@ -23,29 +23,13 @@ export MPLCONFIGDIR=../mpl
 source ./ttt/bin/activate
 
 
-depth=7 # number of repetitions of layers
+#depth=7 # number of repetitions of layers
 #steps=20 # number of learning steps per epoch
-epochs=50 # number of epochs
+#epochs=50 # number of epochs
 #points=30 # number of points to compute gradient
-statistics=19 # number of runs for each parameter combination -1
+#statistics=19 # number of runs for each parameter combination -1
 
-declare -a parameters
-
-for ce in "true" "false"; do
-    for points in $(seq 10 10 40); do
-        for steps in $(seq 20 20 100); do
-            for ss in $(seq 0.004 0.002 0.012); do # sweep step size
-                for i in $(seq 0 1 $statistics); do # 19 is the number of jobs with this parameters
-                    parameters+=("-s true -n ${steps} -p ${points} -l tcemoid -f output/depth_7 -ss ${ss} -sr true -re ${depth} -ep true -epn ${epochs} -ce ${ce}")
-                done
-            done
-        done
-    done
-done
-
-
-
-
+readarray -t parameters < ./param_file_no_eps.txt
 
 echo " "
 echo "##############################################"
