d4fab30655e03c84c1c76fe9fa83fe1aee448a79
Francesco Arzani <frarzani@physik.fu-berlin.de>
2022-04-25 23:28:40 +0200

    adding create_params scripts for editing

--- create_params_symm_lp_sweep_eps.sh
+++ create_params_symm_lp_sweep_eps.sh
@@ -2,19 +2,21 @@
 
 declare -a parameters # this array will be stored in the parameters file for slurm
 
-marker=12 # an index for folders to save results
+marker=2000 # an index for folders to save results
 
 steps=30 # steps per epoch
-statistics=10 # how many runs with identical parameters there will be to acquire statistics
+statistics=4 # how many runs with identical parameters there will be to acquire statistics
 epochs=100 # for running without epochs put -1
 points=15 # data points per step
-
-for ce in "true" "false"; do
-    for reps in $(seq 4 1 5); do # sweeping repetitions of the base layout
-        for ss in $(seq 0.008 0.002 0.01); do # sweep step size
-            #mkdir ./output/jobarray/epochs/${marker} # this is for laptop
+ss=0.008
+ce=false
+#for ce in "true" "false"; do
+    for reps in $(seq 1 1 5); do # sweeping repetitions of the base layout
+        #for ss in $(seq 0.008 0.002 0.01); do # sweep step size
+        for p in "cemoid" "cemoidcemoid" "cemoidcemoidcemoid" "cemoidcemoidcemoidcemoid" "cemoidcemoidcemoidcemoidcemoid" ; do # sweeping repetitions of cmoid layout after each data encoding
+	    #mkdir ./output/jobarray/epochs/${marker} # this is for laptop
             mkdir /scratch/frarzani/pqc_out/epochs/${marker} # this is for cluster
-            paramstr="-s true -n ${steps} -p ${points} -l tcemoidcemoidcemoid -f /scratch/frarzani/pqc_out/epochs/${marker} -ss ${ss} -re ${reps} -ep true -epn ${epochs} -ce ${ce} -cg cry"
+	    paramstr="-s true -n ${steps} -p ${points} -l t${p} -f /scratch/frarzani/pqc_out/epochs/${marker} -ss ${ss} -re ${reps} -ep true -epn ${epochs} -ce ${ce}"
             #printf "%s" "${paramstr}" > ./output/jobarray/no_epochs/${marker}/params.txt # laptop
             printf "%s" "${paramstr}" > /scratch/frarzani/pqc_out/epochs/${marker}/params.txt # cluster
             marker=$((marker+1))
@@ -25,7 +27,7 @@ for ce in "true" "false"; do
             done
         done
     done
-done
+#done
 
 printf "%s\n" "${parameters[@]}" > param_file_eps.txt
 
--- create_params_symm_rand_layout_sweep_eps.sh
+++ create_params_symm_rand_layout_sweep_eps.sh
@@ -2,19 +2,21 @@
 
 declare -a parameters # this array will be stored in the parameters file for slurm
 
-marker=12 # an index for folders to save results
+marker=5000 # an index for folders to save results
 
 steps=30 # steps per epoch
-statistics=10 # how many runs with identical parameters there will be to acquire statistics
+statistics=4 # how many runs with identical parameters there will be to acquire statistics
 epochs=100 # for running without epochs put -1
 points=15 # data points per step
+ce="false"
+ss=0.008
 
-for ce in "true" "false"; do
-    for reps in $(seq 4 1 5); do # sweeping repetitions of the base layout
-        for ss in $(seq 0.008 0.002 0.01); do # sweep step size
-            #mkdir ./output/jobarray/epochs/${marker} # this is for laptop
-            mkdir /scratch/frarzani/pqc_out/epochs/${marker} # this is for cluster
-            paramstr="-s true -n ${steps} -p ${points} -l tcemoidcemoidcemoid -f /scratch/frarzani/pqc_out/epochs/${marker} -ss ${ss} -re ${reps} -ep true -epn ${epochs} -ce ${ce} -cg cry"
+for reps in 3
+do
+       for layout in 'doemcimcdoeiceoidm' 'ecimdoicoemdioedcm' 'dcmoeicmodieoeicdm' 'dmceoidmcoeidciemo' 'oeicdmoidecmcmdieo' 'eomcdimdecoiecdoim' 'deomiceoidmcomedci' 'deomcieicomdcmoide' 
+       do
+	    mkdir /scratch/frarzani/pqc_out/epochs/${marker} # this is for cluster
+            paramstr="-s true -n ${steps} -p ${points} -l t${layout} -f /scratch/frarzani/pqc_out/epochs/${marker} -ss ${ss} -re ${reps} -ep true -epn ${epochs} -ce ${ce} -cg ry"
             #printf "%s" "${paramstr}" > ./output/jobarray/no_epochs/${marker}/params.txt # laptop
             printf "%s" "${paramstr}" > /scratch/frarzani/pqc_out/epochs/${marker}/params.txt # cluster
             marker=$((marker+1))
@@ -23,11 +25,11 @@ for ce in "true" "false"; do
             for i in $(seq 0 1 $statistics); do 
                 parameters+=("${paramstr}") # add as many lines with this set of params as needed to acquire statistics
             done
-        done
-    done
+       done
 done
 
-printf "%s\n" "${parameters[@]}" > param_file_eps.txt
+
+printf "%s\n" "${parameters[@]}" > param_file_eps_rand.txt
 
 #readarray -t parameters < ./param_file_eps.txt
 #echo ${parameters[1]}
