18e07d4ee78f48b387741e3d1d93878b2d69f048
Marian Mularski <marian.mularski@porsche.digital>
2022-04-27 17:24:38 +0200

    update porsche use case stuff

--- pdg.py
+++ pdg.py
@@ -18,8 +18,8 @@ import circuits_ttt as pqc
 from intersection import *
 
 if __name__ == "__main__":
-    args_symmetric = {'c': 2, 'e': 2, 'o': 1, 'm': 2, 'i': 1, 'd': 1, 'ed': 3, 'sd':2}
-    args_asymmetric = {'c': 8, 'e': 8, 'o': 8, 'm': 2, 'i': 4, 'd': 4, 'ed': 9, 'sd':2}
+    args_symmetric = {'c': 2, 'e': 2, 'o': 2, 'm': 2, 'i': 1, 'd': 1, 'z': 3, 'x':2}
+    args_asymmetric = {'c': 8, 'e': 8, 'o': 8, 'm': 2, 'i': 4, 'd': 4, 'z': 9, 'x':2}
     gate_2q = qml.CRX
     rotation_2q = True
 
@@ -30,15 +30,14 @@ def data_encoding(intersection, direction):
     for i, j in enumerate(order):
         qml.RX(fintersection[j], wires=i)
     
-    if direction == ['n']:
+    if direction == 'n':
         qml.RX(0, wires = 9)
-    elif direction == ['e']:
+    elif direction == 'e':
         qml.RX(np.pi/2, wires = 9)
-    elif direction == ['s']:
+    elif direction == 's':
         qml.RX(np.pi, wires = 9)
-    elif direction == ['w']:
+    elif direction == 'w':
         qml.RX(np.pi*(3/2), wires = 9)
-
     """
     if direction == ['w']:
         qml.RX(np.pi/2, wires = 9)
@@ -49,6 +48,7 @@ def data_encoding(intersection, direction):
     elif direction == ['s']:
         qml.RY(-np.pi/2, wires = 9)
     """
+    
 
 def entangle_direction(param, symm=True):
     
@@ -128,7 +128,7 @@ def outer_layer(param, symm=True):
         if symm:
             for i in range(4):
                     gate_2q(param[0], wires=[corners[i], edges[i]])
-                    gate_2q(param[0], wires=[corners[i], edges[i-1]])
+                    gate_2q(param[1], wires=[corners[i], edges[i-1]])
         else:
             for i in range(4):
                     gate_2q(param[2*i], wires=[corners[i], edges[i]])
@@ -153,8 +153,8 @@ def inner_layer(param, symm=True):
             for i in edges:
                 gate_2q(param[0], wires=[i, 8])
         else:
-            for i in edges:
-                gate_2q(param[i], wires=[i, 8])
+            for n, i in enumerate(edges):
+                gate_2q(param[n], wires=[i, 8])
     else:
         for i in edges:
             gate_2q(wires=[i, 8]) 
@@ -174,8 +174,8 @@ def diag_layer(param, symm=True):
             for i in corners:
                 gate_2q(param[0], wires=[8, i])
         else:
-            for i in corners:
-                gate_2q(param[i], wires=[8, i])
+            for n, i in enumerate(corners):
+                gate_2q(param[n], wires=[8, i])
     else:
         for i in corners:
             gate_2q(wires=[8, i])
@@ -212,7 +212,7 @@ def get_results(result):
     for i in slicer:
         avg_result.append(torch.mean(result[i]).reshape(1))
 
-    result = torch.cat(avg_result)
+    result = (torch.cat(avg_result)+1)/2
     return result
 
 def cost_function_batch(circ, params, games, symmetric, design):
@@ -259,12 +259,12 @@ def translate_to_parameters(design, symmetric=True):
         elif i == 'd':
             # diagonal layer
             param_args.append(param_args[-1]+args['d'])
-        elif i == 'ed':
+        elif i == 'z':
             # entangle direction
-            param_args.append(param_args[-1]+args['ed'])
-        elif i == 'sd':
+            param_args.append(param_args[-1]+args['z'])
+        elif i == 'x':
             # direction
-            param_args.append(param_args[-1]+args['sd'])
+            param_args.append(param_args[-1]+args['x'])
         
     return param_args
 
@@ -287,8 +287,31 @@ def circuit(situation, params, symmetric, design="tceocem tceicem tcedcem"):
     # TODO: this should automatically start from the all-zero state in comp basis right?
 
     args = translate_to_parameters(design, symmetric)
-    ngame = np.pi*situation[0] # normalize entries of game so they are between -pi/2, pi/2
-    direction = situation[1]
+
+    road = situation[0].copy()
+
+    if True:
+        pos = (np.argwhere(road == 0))[0]
+        north = pos + np.array([-1, 0])
+        south = pos + np.array([1, 0])
+        west = pos + np.array([0, -1])
+        east = pos + np.array([0, 1])
+
+        road[pos[0], pos[1]] = -1/3
+        if situation[1] == 'n':
+            road[north[0], north[1]] = 1/3
+        elif situation[1] == 's':
+            road[south[0], south[1]] = 1/3
+        elif situation[1] == 'w':
+            road[west[0], west[1]] = 1/3
+        elif situation[1] == 'e':
+            road[east[0], east[1]] = 1/3
+        
+        direction = 'x'
+    else:
+        direction = situation[1]
+
+    ngame = np.pi*road*(2/3) # normalize entries of game so they are between -pi/2, pi/2
 
     for r in range(params.shape[0]): # r repetitions
 
@@ -307,15 +330,28 @@ def circuit(situation, params, symmetric, design="tceocem tceicem tcedcem"):
                 inner_layer(params[r, args[n]:args[n+1]], symmetric) 
             elif i == 'd':
                 diag_layer(params[r, args[n]:args[n+1]], symmetric)
-            elif i == 'ed':
+            elif i == 'z':
                 entangle_direction(params[r, args[n]:args[n+1]], symmetric)
-            elif i == 'sd':
+            elif i == 'x':
                 sq_direction(params[r, args[n]:args[n+1]])
 
     return qml.expval(qml.PauliZ(8))
+    #return [qml.expval(qml.PauliZ(i)) for i in range(9)]
 
 games_data,labels = make_data()
-# %%
+
+games_data = [games_data[i] for i in range(len(games_data)) if not labels[i] == ['s']]
+
+def fill_list(ls, size):
+    if len(ls) == size:
+        return
+    elif len(ls) > size:
+        raise ValueError('list already larger than requested size')
+    
+    ls += (size%len(ls)-1)*ls
+    while len(ls) < size:
+        ls += [random.choice(ls)]
+
 def gen_games_sample(size, output = None):
     '''
     Generates Tensor with 3*size games that are won equally by X, O and 0
@@ -323,11 +359,26 @@ def gen_games_sample(size, output = None):
 
     param wins: list of wins to be included in sample. If empty, returns completely random sample.
     '''
-    sample = random.sample(games_data, size)
+    sample = []
+    for k in [-1, -0.6, -0.2, 0.2, 0.6, 1]:
+        sample_temp = [i for i in games_data if get_diff(i) == k]
+        try:
+            sample += random.sample(sample_temp, size)
+        except ValueError:
+            print(f'Not enough data points for difficulty {k} \nNumber of data points: {len(sample_temp)}')
+            fill_list(sample_temp, size)
+            sample += sample_temp
+
     sample_difficulty = [get_diff(i) for i in sample]
 
+    if not output == None:
+        with open('samples_pdg/'+output+'.npy', 'wb') as f:
+                np.save(f,np.tensor(sample, requires_grad=False))#, sample_label = sample_label)
+
+
     return sample, np.tensor(sample_difficulty, requires_grad=False)
-# %%
+
+
 def random_params(repetitions, symmetric, design):
     '''
     returns array/torch tensor of paramters for amount of repetitions and circuit design
@@ -336,10 +387,10 @@ def random_params(repetitions, symmetric, design):
 
     return torch.tensor(params, requires_grad = True)
 
-pdg_dev = qml.device("default.qubit", wires=11)
-full_circ_torch = qml.QNode(circuit, pdg_dev, interface='torch')#, diff_method='adjoint')
+pdg_dev = qml.device("lightning.qubit", wires=10)
+full_circ_torch = qml.QNode(circuit, pdg_dev, interface='torch', diff_method='adjoint')
 rng = np.random.default_rng(2021)
-# %%
+
 class road_detection():
 
     def __init__(self, symmetric=True, sample_size=5, design="tcemoidcemoidtcmoid", data_file=None, random_sample=False):
@@ -379,34 +430,35 @@ class road_detection():
         Currently, load is implemented via numpy.load(file.npz)
         '''
         try:
-            with open('samples/'+data_file+'.npz', 'rb') as f:
+            with open('samples_pdg/'+data_file+'.npy', 'rb') as f:
                             print('Loading data file \n')
-                            self.games_sample = np.tensor(np.load(f, allow_pickle = True)['sample'][:size*3], requires_grad=False)
+                            self.games_sample = [[i[0], str(i[1])] for i in np.tensor(np.load(f, allow_pickle=True), requires_grad=False)]
                             #self.label_sample = np.tensor(np.load(f, allow_pickle = True)['sample_label'][:size*3], requires_grad=False)
         except IOError: 
             print('Data sample not found, creating new one')
             self.sample_games(size)
 
-    def run_epochs(self, epochs, samplesize_per_step, steps_per_epoch, stepsize, multiplicator=1, data_file = None):
+    def run_epochs(self, epochs, samplesize_per_step, steps_per_epoch, stepsize,  data_file = None):
         """
         Runs lbfgs training with different epochs
         """
         print('running epochs...')
         print(tabulate([['epochs', epochs], ['stepsize', stepsize], ['symmetric', self.symmetric], ['design', self.design], ['sample size per step', samplesize_per_step], ['steps per epoch', steps_per_epoch], \
-            ['wins', self.wins], ['repetitions', self.repetitions]]))
+            ['repetitions', self.repetitions]]))
 
         self.epochs = True
 
         if data_file is None:
-            self.batch = gen_games_sample(steps_per_epoch*samplesize_per_step, truesize=True, reduced = self.reduced, wins=self.wins)[0]
+            self.batch = gen_games_sample(int(np.ceil(steps_per_epoch*samplesize_per_step/6)))[0]
         else:
             with open('samples/'+data_file+'.npz', 'rb') as f:
                             print('Loading data file \n')
                             self.batch = np.tensor(np.load(f, allow_pickle = True)['sample'], requires_grad=False)
 
-        self.test_batch = gen_games_sample(200)[0]
+        self.test_batch = gen_games_sample(50)[0]
 
         np.random.shuffle(self.batch)
+        self.batch = self.batch[:steps_per_epoch*samplesize_per_step]
 
         self.interface = 'torch'
 
@@ -444,7 +496,7 @@ class road_detection():
                 self.steps = j
             
             np.random.shuffle(self.batch)
-            print(f'unique games: {len(np.unique(self.batch, axis=0))}')
+            #print(f'unique games: {len(np.unique(self.batch, axis=0))}')
             self.theta = self.opt.param_groups[0]['params'][0]  
             print(f'epoch {i}/{epochs} accuracy:')
             self.epoch_accuracy.append(self.check_accuracy(check_batch=self.batch))
@@ -466,10 +518,15 @@ class road_detection():
         print(tabulate([['steps', steps], ['stepsize', stepsize], ['symmetric', self.symmetric], ['design', self.design], ['sample size', len(self.games_sample)], \
             ['random sample', self.random], ['repetitions', self.repetitions]]))
 
+        self.epochs = False
         self.interface = 'torch'
         if not resume:
             self.gd_cost = []
             self.theta = self.init_params_torch
+            self.total_accuracy = []
+            self.training_accuracy = []
+
+        self.test_batch = gen_games_sample(30)[0]
 
         self.opt = torch.optim.LBFGS([self.theta], lr=stepsize)
         self.stepsize = stepsize
@@ -481,6 +538,9 @@ class road_detection():
             loss = self.cost_function(full_circ_torch, self.theta, self.games_sample, self.symmetric, self.design)
             loss.backward()
             return loss
+
+        self.training_accuracy.append(self.check_accuracy(check_batch=self.games_sample))
+        self.total_accuracy.append(self.check_accuracy(check_batch=self.test_batch))
 
         for j in range(steps):
 
@@ -496,26 +556,35 @@ class road_detection():
                 self.sample_games(self.sample_size)
 
             self.gd_cost.append(cost_temp) 
+            self.theta = self.opt.param_groups[0]['params'][0]
+            self.training_accuracy.append(self.check_accuracy(check_batch=self.games_sample))
+            self.total_accuracy.append(self.check_accuracy(check_batch=self.test_batch))
+
             self.steps = j
         
         cost_temp = self.cost_function(full_circ_torch ,self.opt.param_groups[0]['params'][0],self.games_sample, self.symmetric, self.design)
+        self.gd_cost.append(cost_temp)
         print(f"final step current cost value: {cost_temp}")
 
         self.theta = self.opt.param_groups[0]['params'][0]  
+        print('accuracy on training data set:')
+        #self.check_accuracy(check_batch=self.games_sample)
 
-    def check_accuracy(self, check_size=100, check_batch=None):
+    
+    def check_accuracy(self, check_size=30, check_batch=None):
 
         if check_batch is None:
             check_batch, diff_check = gen_games_sample(check_size)
-        
+        else:
+            diff_check = [get_diff(i) for i in check_batch]
         results = []
         targets = []
         for i, situation in enumerate(check_batch):
 
-            result = full_circ_torch(situation, self.thetalm, self.symmetric, self.design).detach().numpy()
+            result = full_circ_torch(situation, self.theta, self.symmetric, self.design).detach().numpy()
 
-            results.append(np.round((result+0.2)*5)/2)
-            targets.append(np.round((diff_check[i]+0.2)*5)/2)
+            results.append(np.round((result+0.2)*5/2))
+            targets.append(np.round((diff_check[i]+0.2)*5/2))
 
         self.confusion_matrix = confusion_matrix(targets, results, normalize='true')
         print('confusion matrix:')
@@ -525,12 +594,19 @@ class road_detection():
         return self.confusion_matrix
   
     """
-    def check_accuracy_old(self, check_size = 100): # TODO: check if accuracy varies for same run
+    def check_accuracy(self, check_size = 12, check_batch=None): # TODO: check if accuracy varies for same run
         # TODO: implement confusion matrix
         '''
         checks accuracy of current theta by sampling check_size amount of games for each win
         '''
-        games_check, labels_check = gen_games_sample(check_size)
+
+        if check_batch is None:
+            games_check = gen_games_sample(check_size)[0]
+        else:
+            games_check = check_batch
+
+        labels_check = [get_label(i) for i in games_check]
+        
         res_circ = []
         res_true = []
 
@@ -572,7 +648,6 @@ class road_detection():
         #self.accuracy[0] = len([j for j in results_alt[0] if j[1] > j[0] and j[1] > j[2]])/len(results_alt[0])
         #self.accuracy[1] = len([j for j in results_alt[1] if j[2] > j[1] and j[2] > j[0]])/len(results_alt[1])
     """
-
     def plot_cost(self):
         '''
         plots cost function
@@ -588,8 +663,8 @@ class road_detection():
         params_tmp = self.init_params_torch.detach().numpy()
         theta_tmp = self.theta.detach().numpy()
 
-        to_save = {'symmetric': self.symmetric,'accuracy': self.confusion_matrix,'execution time': exec_time, 'steps': self.steps, 'stepsize': self.stepsize, 'design': self.design, 'cost function': self.gd_cost, 'sample size': self.sample_size, \
-        'initial parameters': params_tmp, 'sampled games': self.games_sample.numpy(), 'theta': theta_tmp}
+        to_save = {'symmetric': self.symmetric, 'total accuracy': self.total_accuracy, 'training accuracy': self.training_accuracy, 'accuracy': self.confusion_matrix,'execution time': exec_time, 'steps': self.steps, 'stepsize': self.stepsize, 'design': self.design, 'cost function': self.gd_cost, 'sample size': self.sample_size, \
+        'initial parameters': params_tmp, 'sampled games': self.games_sample, 'theta': theta_tmp}
         if self.epochs:
             to_save['epoch cost'] = self.epoch_cost_function
             to_save['epoch accuracy'] = self.epoch_accuracy
