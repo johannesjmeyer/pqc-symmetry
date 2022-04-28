147f46a4aa8a94761ce14b1294925dfb28cfdf40
Francesco Arzani <frarzani@physik.fu-berlin.de>
2022-04-28 17:35:38 +0200

    jobarray scripts for pdg lp sweep

--- pdg_jobarray_Nsymm_lp_sweep.sh
+++ pdg_jobarray_Nsymm_lp_sweep.sh
@@ -11,7 +11,7 @@
 #SBATCH --mail-user=frarzani@physik.fu-berlin.de # Email to which notifications will be sent
 #SBATCH --mail-type=ALL                      # Type of email notification: BEGIN,END,FAIL,ALL
 #
-#SBATCH --array=0-209                 # Array of X jobs id 0..X-1, each using the allocation above, Y of which running concurrently
+#SBATCH --array=0-199                 # Array of X jobs id 0..X-1, each using the allocation above, Y of which running concurrently
                                          # REMEMBER TO CHANGE X DEPENDING ON THE PARAMETERS TO SWEEP
 
 #source /net/opt/spack/buster/spack-dev/opt/linux-debian10-ivybridge/gcc-8.3.0/lmod-8.6.5-vscnztx6cokd2oibqjvon7ykfwf4tumj/lmod/lmod/init/bash
@@ -23,7 +23,7 @@ export MPLCONFIGDIR=../mpl
 source ./ttt/bin/activate
 
 
-readarray -t parameters < ./param_file_pdg_lp_Nsymm.txt
+readarray -t parameters < ./param_file_pdg_Nsymm_rand.txt
 
 
 /usr/bin/time -f "\t%E real,\t%M kb MaxMem" /home/frarzani/pqc-symmetry/ttt/bin/python3 -u run_pdg.py ${parameters[$SLURM_ARRAY_TASK_ID]}
--- pdg_jobarray_symm_lp_sweep.sh
+++ pdg_jobarray_symm_lp_sweep.sh
@@ -23,7 +23,7 @@ export MPLCONFIGDIR=../mpl
 source ./ttt/bin/activate
 
 
-readarray -t parameters < ./param_file_pdg_lp_Nsymm.txt
+readarray -t parameters < ./param_file_pdg_lp_symm.txt
 
 
 /usr/bin/time -f "\t%E real,\t%M kb MaxMem" /home/frarzani/pqc-symmetry/ttt/bin/python3 -u run_pdg.py ${parameters[$SLURM_ARRAY_TASK_ID]}
