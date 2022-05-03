a46cc5798a364970aaa5dafb693e92b798278945
Marian Mularski <marian.mularski@porsche.digital>
2022-05-03 13:55:44 +0200

    add cluster script for random symmetry sweep

---
+++ create_params_symm_rand_symmetry_sweep.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+declare -a parameters # this array will be stored in the parameters file for slurm
+
+marker=14000 # an index for folders to save results
+
+steps=30 # steps per epoch
+statistics=9 # how many runs with identical parameters there will be to acquire statistics
+epochs=100 # for running without epochs put -1
+points=15 # data points per step
+ce="false"
+ss=0.008
+
+for reps in 3
+do
+       for layout in '024613578' '431762058' '358762410' '087521463' '072418653' '521480673' '243608517' '702635184' '142576803' '718523046' '875042361' '760324581' '812506743' '057468132' '631427850' '726180543' '158230674' '613207584' '467180523' '140827563' '473026158' '376480152' '086423715' '381427056' '452378061' '153260847' '386127450' '538604172' '682745103' '645012738' '276140358' 
+       do
+	    mkdir /scratch/frarzani/pqc_out/epochs/${marker} # this is for cluster
+            paramstr="-s true -n ${steps} -p ${points} -l tcemoidcemoidcemoidcemoidcemoid -f /scratch/frarzani/pqc_out/epochs/${marker} -ss ${ss} -re 2 -ep true -epn ${epochs} -ce ${ce} -cg ry -sq ${layout}"
+            #printf "%s" "${paramstr}" > ./output/jobarray/no_epochs/${marker}/params.txt # laptop
+            printf "%s" "${paramstr}" > /scratch/frarzani/pqc_out/epochs/${marker}/params.txt # cluster
+            marker=$((marker+1))
+            #echo ${marker}
+            #echo ${paramstr}
+            for i in $(seq 0 1 $statistics); do 
+                parameters+=("${paramstr}") # add as many lines with this set of params as needed to acquire statistics
+            done
+       done
+done
+
+
+printf "%s\n" "${parameters[@]}" > param_file_eps_symm_rand_symmetry.txt
+
+#readarray -t parameters < ./param_file_eps.txt
+#echo ${parameters[1]}
---
+++ jobarray_Nsymm_rand_symmetry.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+#SBATCH --time=1-00:00:00                    # Runtime in DAYS-HH:MM:SS format
+#
+#SBATCH --ntasks=1                           # Number of processes
+#SBATCH --cpus-per-task=8                    # Number of cores
+#SBATCH --mem-per-cpu=15000                    # Memory per cpu in MB (see also --mem)
+#
+#SBATCH --chdir=/home/frarzani/pqc-symmetry         #    THIS ASSUMES THAT THE SAVE PATH IS GIVEN WITHIN run.py
+#
+#SBATCH --mail-user=frarzani@physik.fu-berlin.de # Email to which notifications will be sent
+#SBATCH --mail-type=ALL                      # Type of email notification: BEGIN,END,FAIL,ALL
+#
+#SBATCH --array=0-87                 # Array of X jobs id 0..X-1, each using the allocation above, Y of which running concurrently
+                                         # REMEMBER TO CHANGE X DEPENDING ON THE PARAMETERS TO SWEEP
+
+#source /net/opt/spack/buster/spack-dev/opt/linux-debian10-ivybridge/gcc-8.3.0/lmod-8.6.5-vscnztx6cokd2oibqjvon7ykfwf4tumj/lmod/lmod/init/bash
+#module use /net/opt/spack/buster/spack-dev/modules/lmod/linux-debian10-x86_64/Core
+#module load openmpi/4.1.2-y72mjnh
+#source /net/opt/spack/buster/spack-dev/spack/var/spack/environments/buildenv-20220323-1/loads
+
+export MPLCONFIGDIR=../mpl
+source ./ttt/bin/activate
+
+
+#depth=7 # number of repetitions of layers
+#steps=20 # number of learning steps per epoch
+#epochs=50 # number of epochs
+#points=30 # number of points to compute gradient
+#statistics=19 # number of runs for each parameter combination -1
+
+readarray -t parameters < ./param_file_eps_Nsymm_rand_symmetry.txt
+
+#echo " "
+#echo "##############################################"
+#echo "Circuit depth: $depth"  
+#echo "Parallel runs: $parruns"  
+#echo "Toral runs: $datapoints"
+#echo "Starting runs for symmetric case"
+#echo "##############################################"
+#echo " "
+
+/usr/bin/time -f "\t%E real,\t%M kb MaxMem" /home/frarzani/pqc-symmetry/ttt/bin/python3 -u run_ttt.py ${parameters[$SLURM_ARRAY_TASK_ID]}
+
