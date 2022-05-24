fe9990e90b8c4b70db2010141d27d241073f3f7c
Francesco Arzani <frarzani@physik.fu-berlin.de>
2022-05-24 16:30:07 +0200

    more scripts for rerun -- lp sweep ry

--- create_params_Nsymm_lp_sweep_eps.sh
+++ create_params_Nsymm_lp_sweep_eps.sh
@@ -6,7 +6,7 @@ marker=3000 # an index for folders to save results
 
 steps=30 # steps per epoch
 statistics=9 # +1 = how many runs with identical parameters there will be to acquire statistics
-epochs=100 # for running without epochs put -1
+epochs=150 # for running without epochs put -1
 points=15 # data points per step
 ss=0.008
 ce=false
@@ -16,7 +16,7 @@ ce=false
         for p in "cemoid" "cemoidcemoid" "cemoidcemoidcemoid" "cemoidcemoidcemoidcemoid" "cemoidcemoidcemoidcemoidcemoid" ; do # sweeping repetitions of cmoid layout after each data encoding
 	    #mkdir ./output/jobarray/epochs/${marker} # this is for laptop
             mkdir /scratch/frarzani/pqc_out/epochs/${marker} # this is for cluster
-	    paramstr="-s false -n ${steps} -p ${points} -l t${p} -f /scratch/frarzani/pqc_out/epochs/${marker} -ss ${ss} -re ${reps} -ep true -epn ${epochs} -ce ${ce} -cg ry"
+	    paramstr="-s false -n ${steps} -p ${points} -l t${p} -f /scratch/frarzani/pqc_out/epochs/${marker} -ss ${ss} -re ${reps} -ep true -epn ${epochs} -cg ry"
             #printf "%s" "${paramstr}" > ./output/jobarray/no_epochs/${marker}/params.txt # laptop
             printf "%s" "${paramstr}" > /scratch/frarzani/pqc_out/epochs/${marker}/params.txt # cluster
             marker=$((marker+1))
@@ -35,7 +35,7 @@ ce=false
         for p in "cemoid" "cemoidcemoid" "cemoidcemoidcemoid" ; do # sweeping repetitions of cmoid layout after each data encoding
 	    #mkdir ./output/jobarray/epochs/${marker} # this is for laptop
             mkdir /scratch/frarzani/pqc_out/epochs/${marker} # this is for cluster
-	        paramstr="-s false -n ${steps} -p ${points} -l t${p} -f /scratch/frarzani/pqc_out/epochs/${marker} -ss ${ss} -re ${reps} -ep true -epn ${epochs} -ce ${ce} -cg ry"
+	        paramstr="-s false -n ${steps} -p ${points} -l t${p} -f /scratch/frarzani/pqc_out/epochs/${marker} -ss ${ss} -re ${reps} -ep true -epn ${epochs} -cg ry"
             #printf "%s" "${paramstr}" > ./output/jobarray/no_epochs/${marker}/params.txt # laptop
             printf "%s" "${paramstr}" > /scratch/frarzani/pqc_out/epochs/${marker}/params.txt # cluster
             marker=$((marker+1))
