e08c7adac1a840eb8552b881eacba85159740973
Francesco Arzani <frarzani@physik.fu-berlin.de>
2022-05-24 11:42:52 +0200

    moved scripts to rerun rand layout

--- create_params_symm_rand_layout_sweep_eps_y.sh
+++ create_params_symm_rand_layout_sweep_eps_y.sh
@@ -6,7 +6,7 @@ marker=4000 # an index for folders to save results
 
 steps=30 # steps per epoch
 statistics=9 # how many runs with identical parameters there will be to acquire statistics
-epochs=100 # for running without epochs put -1
+epochs=150 # for running without epochs put -1
 points=15 # data points per step
 ce="false"
 ss=0.008
@@ -16,7 +16,7 @@ do
         for layout in 'eiomcdmoedicoidmce' 'iemocdeomdicmodcie' 'eocmidomidecoimedc' 'cmodiecioemdcdmeio' 'iodmcedmeoicocidem' 'ecmodidecmioidcmeo' 'ecmidodeicomimdeco' 'emcoidceidomocedmi' 'eimdcocodeimcoemdi' 'eiodmcieomcdoeidmc' 'edoicmecdiomioecdm' 'cdeomideoimcmdcioe' 'doemcimcdoeiceoidm' 'ecimdoicoemdioedcm' 'dcmoeicmodieoeicdm' 'dmceoidmcoeidciemo' 'oeicdmoidecmcmdieo' 'eomcdimdecoiecdoim' 'deomiceoidmcomedci' 'deomcieicomdcmoide'        
         do
 	    mkdir /scratch/frarzani/pqc_out/epochs/${marker} # this is for cluster
-            paramstr="-s true -n ${steps} -p ${points} -l t${layout} -f /scratch/frarzani/pqc_out/epochs/${marker} -ss ${ss} -re ${reps} -ep true -epn ${epochs} -ce ${ce} -cg ry"
+            paramstr="-s true -n ${steps} -p ${points} -l t${layout} -f /scratch/frarzani/pqc_out/epochs/${marker} -ss ${ss} -re ${reps} -ep true -epn ${epochs} -cg ry"
             #printf "%s" "${paramstr}" > ./output/jobarray/no_epochs/${marker}/params.txt # laptop
             printf "%s" "${paramstr}" > /scratch/frarzani/pqc_out/epochs/${marker}/params.txt # cluster
             marker=$((marker+1))
